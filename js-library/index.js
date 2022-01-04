4aa44d77d5982e922d5d5a14c6e0f2cad5af63f0
1zemzem <1zemliakova@gmail.com>
2022-01-04 09:28:49 +0300

    array-methods

--- js-library/index.js
+++ js-library/index.js
@@ -1,14 +1,38 @@
-import {getValue, getElement} from "./modules/array-methods.js";
+import {
+  getValue,
+  getElement,
+  changeRegister,
+  getScores,
+  getScoresMoreThan,
+  getSum,
+  getIndex,
+  getLastElement,
+  getFirstElement,
+  getMaxNumber,
+  getMinNumber,
+  asChain,
+} from "./modules/array-methods.js";
 
-
-const clients = [ 
-    { id: 2, name: "Alex" },
-    { id: 3, name: "Peter" },
-    { id: 4, name: "Mike" },
-    { id: 5, name: "Nick" },
+const clients = [
+  { id: 2, name: "Alex", age: 12, scores: 101 },
+  { id: 3, name: "Peter", age: 18, scores: 105 },
+  { id: 4, name: "Mike", age: 22, scores: 99 },
+  { id: 5, name: "Nick", age: 19, scores: 100 },
 ];
-    
 
-console.log(getValue(clients, 'name'));
-console.log(getElement(clients, 'name'));
+const str = "Hello World";
 
+const numbers = [1, 2, 3, 4, 5, 5, 5, 6, 7, 8, 9, 525, -525, 10];
+
+console.log(getValue(clients, "name"));
+console.log(getElement(clients, "name"));
+console.log(changeRegister(str));
+console.log(getScores(clients));
+console.log(getScoresMoreThan(clients));
+console.log(getSum(numbers));
+console.log(getIndex(numbers, 7));
+console.log(getFirstElement(numbers))
+console.log(getLastElement(numbers));
+console.log(getMaxNumber(numbers));
+console.log(getMinNumber(numbers));
+console.log(asChain(numbers));
