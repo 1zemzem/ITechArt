6765de4c8801647ba1452a71418e0692397bd940
1zemzem <1zemliakova@gmail.com>
2022-01-31 11:41:54 +0300

    ff

--- src/components/current-wether/current-weather.jsx
+++ src/components/current-wether/current-weather.jsx
@@ -61,6 +61,7 @@ export default function CurrentWeather({
             <div className="container-data__info-row-value">
               {main.feels_like} °C
             </div>
+            
           </div>
           <div className="container-data__info-row">
             <div className="container-data__info-row-label">Visibility</div>
