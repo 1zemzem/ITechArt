66dc70f2182198072c10ea27057cf3fb260e4d71
1zemzem <1zemliakova@gmail.com>
2022-01-31 16:04:25 +0300

    current-weather

---
+++ my-weather-app/src/App/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.scss";
+import Main from "../components/main/main";
+
+// const apiKey = '58b6f7c78582bffab3936dac99c31b25';
+// const getCurrentApiUrl = (city, apiKey) => `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+// const getWe = async(getCurrentApiUrl) => {
+//   const response = await fetch(getCurrentApiUrl(city, apiKey));
+//   const data = await response.json();
+//   console.log(data);
+// }
+
+function App() {
+  return (
+    <div className="app">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
+
+
